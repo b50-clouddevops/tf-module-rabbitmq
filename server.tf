23c5a6b9f73a74dd123143ad022d4e546c865cdf
mvpc-11232 <mvp2612@hotmail.com>
2022-11-15 07:22:47 +0530

    Auto Push Commit

--- server.tf
+++ server.tf
@@ -1,10 +1,10 @@
 # Creates spot instances
 resource "aws_spot_instance_request" "spot" {
   ami                       = data.aws_ami.my_ami.id
-  instance_type             = var.INSTANCE_TYPE
+  instance_type             = "t3.micro"
   wait_for_fulfillment      = true 
-  vpc_security_group_ids    = [aws_security_group.allow_app.id]
-  subnet_id                 = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  vpc_security_group_ids    = [aws_security_group.allow_rabbitmq.id]
+  subnet_id                 = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS[0]
   iam_instance_profile      = "b50-admin"
 
   tags = {
@@ -13,4 +13,18 @@ resource "aws_spot_instance_request" "spot" {
 }
 
 
+resource "null_resource" "application_deploy" {
+    provisioner "remote-exec" {  
+    connection {
+        type     = "ssh"
+        user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
+        password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
+        host     = element(local.ALL_INSTANCE_PRIVATE_IPS, count.index)
+    }
 
+        inline = [
+
+        "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} -e MONGO_URL=${data.terraform_remote_state.db.outputs.MONGODB_ENDPOINT}  roboshop-pull.yml"
+            ]
+        }
+    }
