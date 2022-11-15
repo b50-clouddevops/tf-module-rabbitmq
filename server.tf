2475d5d3f71ffaac840139f18ab3d86f10cb5bfa
mvpc-11232 <mvp2612@hotmail.com>
2022-11-15 07:26:35 +0530

    Auto Push Commit

--- server.tf
+++ server.tf
@@ -1,5 +1,5 @@
 # Creates spot instances
-resource "aws_spot_instance_request" "spot" {
+resource "aws_spot_instance_request" "rabbitmq" {
   ami                       = data.aws_ami.my_ami.id
   instance_type             = "t3.micro"
   wait_for_fulfillment      = true 
