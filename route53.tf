e2cc0647edefe7ed9be40931eccf51bedfa188e1
mvpc-11232 <mvp2612@hotmail.com>
2022-11-15 07:25:13 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "record" {
-  zone_id = var.LB_TYPE == "internal" ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTEDZONE_ID
-  name    = var.LB_TYPE == "internal" ? "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}" : "${var.COMPONENT}-${var.ENV}"
-  type    = "CNAME"
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
+  name    ="rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"
+  type    = "A"
   ttl     = 10
-  records = var.LB_TYPE == "internal" ?  [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
+  records = aws_spot_instance_request.rabbitmq.private_ip
 }
